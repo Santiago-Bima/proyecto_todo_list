ee54a19d8a70f77b0ceb7c6c1efa52cd9bda4ffa
Santiago-Bima <santiago.bima@gmail.com>
2024-06-26 21:01:26 -0300

    creacion del codigo del controlador de tareas

---
+++ back/controllers/TodoController.js
@@ -0,0 +1,58 @@
+import TodoModel from '../models/TodoModel.js'
+
+export const getAllTodos = async (req, res) => {
+  try {
+    const todos = await TodoModel.findAll()
+    res.json(todos)
+  } catch (error) {
+    res.json({message: error.message})
+  }
+}
+
+export const getTodo = async (req, res) => {
+  try {
+    const todo = await TodoModel.findAll({
+      where:{ id:req.params.id }
+    })
+    res.json(todo)
+  } catch (error) {
+    res.json({message: error.message})
+  }
+}
+
+export const createTodo = async (req,res) => {
+  try {
+    await TodoModel.create(req.body)
+    res.json({
+      'message': 'La tarea fue creada correctamente'
+    })
+  } catch (error) {
+    res.json({message: error.message})
+  }
+}
+
+export const updateTodo = async (res,req) => {
+  try {
+    await TodoModel.update(req.body, {
+      where: { id: req.params.id }
+    })
+    res.json({
+      'message': 'La tarea fue editada correctamente'
+    })
+  } catch (error) {
+    res.json({message: error.message})
+  }
+}
+
+export const deleteTodo = async (req,res) => {
+  try {
+    await TodoModel.destroy({
+      where: { id: req.params.id }
+    })
+    res.json({
+      'message': 'La tarea fue eliminada correctamente'
+    })
+  } catch (error) {
+    res.json({message: error.message})
+  }
+}
